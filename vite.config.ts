d6ad7ed39c58a1a974555fabeab0dfe0f0943a8d
Daofeng Wu <dolphin.w.e@gmail.com>
2023-10-27 15:42:58 -0400

    ci: fix vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/CS541-DDPoetry/' : '',
   plugins: [react(), tsconfigPaths()],
   css: {
     modules: {
       localsConvention: 'camelCase',
     },
-  }
-})
+  },
+});
